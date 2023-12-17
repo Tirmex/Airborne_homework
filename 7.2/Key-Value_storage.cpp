3376df1011f5e7f99b89480189bd73deb8b9caa2
ananasik558 <denchickshow@list.ru>
2023-12-17 12:15:05 +0300

    my task

---
+++ 7.2/Key-Value_storage.cpp
@@ -0,0 +1,8 @@
+template <typename Key, typename Value>
+bool KeyValueStorage<Key, Value>::Find(const Key& key, Value* const value) const {
+    auto it = data.find(key);
+    if (it != data.end() && value != nullptr) {
+        *value = it->second;
+    }
+    return it != data.end();
+}
